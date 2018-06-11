95429ab94e9b8534ec6aec4b889ffcc35afa59d0
LordCheddar <sean@ydangleapps.com>
2018-06-11 13:47:44 +0200

    add basic jwt decoding

---
+++ core/src/main/java/io/blockv/core/internal/net/rest/auth/JwtDecoderImpl.kt
@@ -0,0 +1,26 @@
+package io.blockv.core.internal.net.rest.auth
+
+import android.util.Base64
+import android.util.Log
+import io.blockv.core.model.DecodedJwt
+import io.blockv.core.model.Jwt
+import org.json.JSONObject
+import java.util.*
+
+class JwtDecoderImpl : JwtDecoder {
+  override fun decode(jwt: Jwt): DecodedJwt {
+    val parts: List<String> = jwt.token.split(".")
+    try {
+
+      if (parts.size == 3) {
+        val data = JSONObject(String(Base64.decode(parts[1].toByteArray(), Base64.DEFAULT)))
+        return DecodedJwt(jwt, data.getString("user_id"), Date(data.getLong("exp")))
+      }
+    } catch (exception: Exception) {
+      Log.e("JwtDecoder",exception.message);
+    }
+    throw InvalidTokenException()
+  }
+
+  class InvalidTokenException : Exception("Invalid Token")
+}
