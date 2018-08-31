000af8ae9cb583f5020608e0c2678c05a8e622a8
LordCheddar <lordcheddar@gmail.com>
2018-08-31 17:13:21 +0200

    remove emit params

--- face/src/main/java/io/blockv/face/client/ViewFactory.kt
+++ face/src/main/java/io/blockv/face/client/ViewFactory.kt
@@ -1,22 +1,19 @@
 package io.blockv.face.client
 
-import io.blockv.core.model.Face
-import io.blockv.core.model.Vatom
-
 interface ViewFactory {
 
   val displayUrl: String
 
-  fun emit(vatom: Vatom, face: Face, bridge: FaceBridge?): FaceView
+  fun emit(): FaceView
 
   companion object {
 
-    fun wrap(displayUrl: String, emitter: (vatom: Vatom, face: Face, bridge: FaceBridge?) -> FaceView): ViewFactory {
+    fun wrap(displayUrl: String, emitter: () -> FaceView): ViewFactory {
 
       return object : ViewFactory {
 
-        override fun emit(vatom: Vatom, face: Face, bridge: FaceBridge?): FaceView {
-          return emitter.invoke(vatom, face, bridge)
+        override fun emit(): FaceView {
+          return emitter.invoke()
         }
 
         override val displayUrl: String
