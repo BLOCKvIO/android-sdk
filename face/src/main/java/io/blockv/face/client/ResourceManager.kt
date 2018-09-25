e14881fece2a2d24c136c4fe2c61497635b545eb
LordCheddar <lordcheddar@gmail.com>
2018-09-25 13:37:18 +0200

    add intial version of the reosurce manager

---
+++ face/src/main/java/io/blockv/face/client/ResourceManager.kt
@@ -0,0 +1,19 @@
+package io.blockv.face.client
+
+import android.graphics.Bitmap
+import io.blockv.common.model.Resource
+import io.blockv.common.util.Callable
+import java.io.File
+import java.io.InputStream
+
+interface ResourceManager {
+
+  fun getFile(resource: Resource): Callable<File>
+
+  fun getInputStream(resource: Resource): Callable<InputStream>
+
+  fun getBitmap(resource: Resource): Callable<Bitmap>
+
+  fun getBitmap(resource: Resource, width: Int, height: Int): Callable<Bitmap>
+
+}
