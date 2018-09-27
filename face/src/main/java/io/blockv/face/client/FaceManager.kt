95925b7618bbc09cca1441139307879a976b5a76
LordCheddar <lordcheddar@gmail.com>
2018-09-27 13:35:11 +0200

    add inline docs

--- face/src/main/java/io/blockv/face/client/FaceManager.kt
+++ face/src/main/java/io/blockv/face/client/FaceManager.kt
@@ -6,9 +6,26 @@ import android.view.ViewGroup
 import io.blockv.common.model.Face
 import io.blockv.common.model.Vatom
 import io.blockv.common.util.Callable
+import io.blockv.face.client.FaceManager.EmbeddedProcedure.*
 
 interface FaceManager {
 
+  /**
+   * EmbeddedProcedure enum contains a set of default face selection procedures.
+   *
+   * By default these procedures will only look at native faces that have a platform value 'Generic'
+   * or 'Android'.
+   *
+   * Faces with a platform value of 'Android' will be selected over 'Generic' in the case where there are
+   * multiple faces per view mode.
+   *
+   * @see ICON - Selects a Face model that is in icon mode, if none is available it returns null.
+   * @see ENGAGED - Selects a Face model in engaged mode,if none is available it falls back on ICON procedure.
+   * @see FULLSCREEN - Selects a Face model that is in fullscreen mode, if none is available it returns null.
+   * @see CARD - Selects a Face model that is card mode, if none is available it returns null.
+   *
+   * @see FaceSelectionProcedure
+   */
   enum class EmbeddedProcedure(val viewMode: String, val fallback: EmbeddedProcedure?) {
 
     ICON("icon", null),
@@ -22,11 +39,11 @@ interface FaceManager {
         return object : FaceManager.FaceSelectionProcedure {
           override fun select(
             vatom: Vatom,
-            faceRegistry: Set<String>
+            displayUrls: Set<String>
           ): Face? {
             var context: EmbeddedProcedure? = procedure
             do {
-              val face = defaultRoutine(vatom.faces, faceRegistry, context!!.viewMode)
+              val face = defaultRoutine(vatom.faces, displayUrls, context!!.viewMode)
               if (face != null)
                 return face
               context = context.fallback
@@ -78,32 +95,135 @@ interface FaceManager {
     }
   }
 
+  /**
+   * Adds a FaceView factory to the roster. If a factory with the same display url already exists
+   * this will overwrite it.
+   *
+   * @param factory is responsible for emitting a FaceView for a specific display url.
+   * @see FaceView
+   * @see ViewFactory
+   */
   fun registerFace(factory: ViewFactory)
 
+  /**
+   * A map of all available FaceView factories.
+   *
+   * @see ViewFactory
+   */
   val faceRoster: Map<String, ViewFactory>
 
+  /**
+   * The default loading view to be used by the VatomView.
+   *
+   * @see ViewEmitter
+   */
   var defaultLoader: ViewEmitter?
 
+  /**
+   * The default error view to be used by the VatomView.
+   *
+   * @see ViewEmitter
+   */
   var defaultError: ViewEmitter?
 
+  /**
+   * FaceSelectionProcedure is used to select a Face for a vAtom.
+   *
+   * @see EmbeddedProcedure
+   */
   interface FaceSelectionProcedure {
-    fun select(vatom: Vatom, faceRegistry: Set<String>): Face?
+
+    /**
+     * Selects a Face model for a vAtom base on some custom logic.
+     *
+     * @param vatom contains the list Face models that should be used by the procedure.
+     * @param displayUrls is a set of all available FaceView factories.
+     * @return an optional Face model.
+     *
+     * @see Vatom
+     * @see Vatom.faces
+     * @see Face
+     */
+    fun select(vatom: Vatom, displayUrls: Set<String>): Face?
   }
 
+  /**
+   * Creates a builder to load a VatomView for the specified vAtom.
+   *
+   * @param vatom is the vAtom you want a VatomView to display.
+   * @return a new Builder.
+   *
+   * @see Builder
+   */
   fun load(vatom: Vatom): Builder
 
+  /**
+   * Builder used to create a Callable to load a VatomView.
+   */
   interface Builder {
 
+    /**
+     * Builds a Callable to load a FaceView into the provided VatomView.
+     *
+     * @param vatomView to load a FaceView for.
+     * @return new Callable<FaceView>, calling this will begin the loading chain.
+     *
+     * @see FaceView
+     * @see VatomView
+     */
     fun into(vatomView: VatomView): Callable<FaceView>
 
+    /**
+     * Sets an embedded procedure to be used to selected a Face model for the vAtom.
+     *
+     * @param embeddedProcedure to be used to select a face.
+     * @return this Builder.
+     *
+     * @see EmbeddedProcedure
+     */
     fun setEmbeddedProcedure(embeddedProcedure: EmbeddedProcedure): Builder
 
-    fun setFaceSelectionProcedure(routine: FaceSelectionProcedure): Builder
+    /**
+     * Set a custom procedure to be used to select a Face model for the vAtom.
+     *
+     * @param procedure to be used to select the Face model.
+     * @return this Builder.
+     *
+     * @see FaceSelectionProcedure
+     */
+    fun setFaceSelectionProcedure(procedure: FaceSelectionProcedure): Builder
 
+    /**
+     * Sets the error view to be used by the VatomView. If this is not set
+     * the default error view will be used.
+     *
+     * @param view to be used as the error view.
+     * @return this Builder.
+     *
+     * @see VatomView
+     * @see defaultError
+     */
     fun setErrorView(view: View): Builder
 
+    /**
+     * Sets the loader view to be used by the VatomView. If this is not set
+     * the default loader view will be used.
+     *
+     * @param view to be used as a loader view.
+     * @return this Builder.
+     *
+     * @see VatomView
+     * @see defaultLoader
+     */
     fun setLoaderView(view: View): Builder
 
+    /**
+     * Sets a delay before the loader be will shown. If the VatomView finishes loading before
+     * the timeout, the loader view will not be shown.
+     *
+     * @param time is the delay in milliseconds.
+     * @return this Builder.
+     */
     fun setLoaderDelay(time: Long): Builder
 
     enum class Error(val message: String) {
@@ -118,8 +238,18 @@ interface FaceManager {
     class VatomViewException(val error: Error) : Exception(error.message)
   }
 
+  /**
+   * View Emitter is used to create custom loader and error views.
+   */
   interface ViewEmitter {
 
+    /**
+     * Creates a view to be used by VatomView.
+     *
+     * Do not attach the view to the parent, this is done during VatomView loading process.
+     *
+     * When using the LayoutInflater set 'attachedToRoot' false, inflater.inflate(<your-resource-id>, parent, false).
+     */
     fun emit(inflater: LayoutInflater, parent: ViewGroup, vatom: Vatom, resourceManager: ResourceManager): View
   }
 
