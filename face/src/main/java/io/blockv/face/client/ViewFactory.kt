e1d853b0d26cd20d2e6615d7016fd585577f9f0c
LordCheddar <lordcheddar@gmail.com>
2018-09-27 15:15:58 +0200

    add inline docs to factory

--- face/src/main/java/io/blockv/face/client/ViewFactory.kt
+++ face/src/main/java/io/blockv/face/client/ViewFactory.kt
@@ -3,10 +3,26 @@ package io.blockv.face.client
 import io.blockv.common.model.Face
 import io.blockv.common.model.Vatom
 
+/**
+ * Used to emit custom FaceViews. Each custom FaceView should have it's own factory.
+ */
 interface ViewFactory {
 
+  /**
+   * The display url of the FaceView, used when registering and selecting a ViewFactory.
+   *
+   * @see FaceManager.registerFace
+   */
   val displayUrl: String
 
+  /**
+   * Emits a new instance of a custom FaceView.
+   *
+   * @param vatom to be displayed.
+   * @param face model that was selected.
+   * @param bridge provides available sdk functions to the FaceView.
+   * @return new FaceView
+   */
   fun emit(
     vatom: Vatom,
     face: Face,
