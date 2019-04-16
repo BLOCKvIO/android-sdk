f56ca95bbb53dbd7ad77a9979d0571b4e6f28134
LordCheddar <lordcheddar@gmail.com>
2019-04-16 19:18:50 +0200

    optimise deserialization

--- common/src/main/java/io/blockv/common/internal/json/serializer/GenericSerializer.kt
+++ common/src/main/java/io/blockv/common/internal/json/serializer/GenericSerializer.kt
@@ -10,17 +10,61 @@
  */
 package io.blockv.common.internal.json.serializer
 
-import android.util.Log
 import io.blockv.common.util.JsonUtil
 import org.json.JSONArray
 import org.json.JSONObject
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.KProperty
-import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.memberProperties
 
 class GenericSerializer : Serializer<Any> {
+
+  val deserializationData = HashMap<KClass<*>, ValueCache>()
+
+  @Synchronized
+  fun getDeserializationData(type: KClass<*>): ValueCache {
+    if (deserializationData.containsKey(type)) {
+      return deserializationData[type]!!
+    } else {
+      val constructor = type.constructors.find { constructor ->
+        constructor.annotations.find {
+          it is Serializer.Serializable
+        } != null
+      } ?: type.constructors.find {
+        it.parameters.isEmpty()
+      }
+      val data = ValueCache(constructor!!)
+
+      for (prop in type.memberProperties) {
+        val annotation = prop.annotations.find {
+          it is Serializer.Serialize
+        }
+        if (annotation != null) {
+          annotation as Serializer.Serialize
+
+          val name = prop.name
+          val fieldType: KClass<*> = prop.returnType.classifier as KClass<*>
+          val jsonPath = annotation.path
+          val jsonName = if (annotation.name.isNotEmpty()) annotation.name else name
+          val default = getDefaultValue(fieldType, annotation.default)
+          val inner = when (fieldType) {
+            List::class -> (prop.returnType.arguments[0].type)?.classifier as KClass<*>
+            Map::class -> (prop.returnType.arguments[1].type)?.classifier as KClass<*>
+            Set::class -> (prop.returnType.arguments[0].type)?.classifier as KClass<*>
+            else -> null
+          }
+          data.values.add(ValueCache.Value(name, jsonName, jsonPath, fieldType, default, inner))
+          if (!prop.isConst && prop is KMutableProperty<*>) {
+            data.props.add(prop)
+          }
+        }
+      }
+      deserializationData[type] = data
+      return data
+    }
+  }
 
   override fun serialize(data: Any, serializers: Map<KClass<*>, Serializer<Any>>): JSONObject? {
     val clss = data::class
@@ -66,33 +110,18 @@ class GenericSerializer : Serializer<Any> {
     if (serializers.containsKey(type)) {
       return serializers[type]!!.deserialize(type, data, serializers)
     }
+    val valueData = getDeserializationData(type)
 
-    val values = getValues(type, data, serializers)
+    val values = getValues(type, data, serializers, valueData)
 
-    val constructor = type.constructors.find { constructor ->
-      constructor.annotations.find {
-        it is Serializer.Serializable
-      } != null
+    val instance = if (valueData.constructor.parameters.isEmpty()) {
+      valueData.constructor.call()
+    } else {
+      valueData.constructor.call(*(valueData.constructor.parameters.map { values[it.name] }.toTypedArray()))
     }
-
-    val instance = constructor?.call(
-      *(constructor.parameters.map { values[it.name] }.toTypedArray())
-    ) ?: type.constructors.find {
-      it.parameters.isEmpty()
-    }?.call() ?: throw Exception("No serializable constructor found -" + type.simpleName)
-
-    type.memberProperties.forEach { prop ->
-
-      val annotation = prop.annotations.find {
-        it is Serializer.Serialize
-      }
-
-      if (annotation != null && !prop.isConst && prop is KMutableProperty<*>) {
-
-        prop.setter.call(instance, values[prop.name])
-      }
+    valueData.props.forEach { prop ->
+      prop.setter.call(instance, values[prop.name])
     }
-
     return instance
   }
 
@@ -190,196 +219,172 @@ class GenericSerializer : Serializer<Any> {
     return out
   }
 
+  fun getDefaultValue(fieldType: KClass<*>, useDefault: Boolean): Any? {
+    if (useDefault) {
+      return when (fieldType) {
+        Boolean::class -> false
+        String::class -> ""
+        Int::class -> 0
+        Float::class -> 0f
+        Double::class -> 0.0
+        Long::class -> 0L
+        JSONObject::class -> JSONObject()
+        JSONArray::class -> JSONArray()
+        List::class -> ArrayList<Any>()
+        Map::class -> HashMap<Any, Any>()
+        Set::class -> HashSet<Any>()
+        else -> null
+      }
+    }
+    return null
+  }
+
   fun getValues(
     type: KClass<*>,
     data: JSONObject,
-    serializers: Map<KClass<*>, Serializer<Any>>
+    serializers: Map<KClass<*>, Serializer<Any>>,
+    valueCache: ValueCache
   ): Map<String, Any?> {
 
     val out = HashMap<String, Any?>()
-    for (prop in type.memberProperties) {
-      val annotation = prop.annotations.find {
-        it is Serializer.Serialize
-      }
+    valueCache.values.forEach { cache ->
+      val name = cache.name
+      val fieldType: KClass<*> = cache.type
+      val json = getJsonObjectForPath(data, cache.jsonPath)
+      val jsonName = cache.jsonName
 
-      if (annotation != null) {
-        annotation as Serializer.Serialize
-        val name = prop.name
-        val fieldType: KClass<*> = prop.returnType.classifier as KClass<*>
-        val json = getJsonObjectForPath(data, annotation.path)
-        val jsonName = if (annotation.name.isNotEmpty()) annotation.name else name
-
-        out[name] =
-          if ((json == null || !json.has(jsonName) || json.isNull(jsonName))) {
-            //handle null/missing json
-            when {
-              annotation.default -> when (fieldType) {
-                Boolean::class -> false
-                String::class -> ""
-                Int::class -> 0
-                Float::class -> 0f
-                Double::class -> 0.0
-                Long::class -> 0L
-                JSONObject::class -> JSONObject()
-                JSONArray::class -> JSONArray()
-                List::class -> ArrayList<Any>()
-                Map::class -> HashMap<Any, Any>()
-                Set::class -> HashSet<Any>()
-                else -> throw Exception("Deserialization Unknown type : " + type.simpleName + " - " + name)
+      out[name] =
+        if ((json == null || !json.has(jsonName) || json.isNull(jsonName))) {
+          //handle null/missing json
+          cache.default
+        } else {
+          if (serializers.containsKey(fieldType)) {
+            serializers[fieldType]!!.deserialize(
+              fieldType,
+              json.getJSONObject(jsonName),
+              serializers
+            )
+          } else
+            when (fieldType) {
+              Boolean::class -> json.getBoolean(jsonName)
+              String::class -> json.getString(jsonName)
+              Int::class -> json.getInt(jsonName)
+              Float::class -> json.getDouble(jsonName).toFloat()
+              Double::class -> json.getDouble(jsonName)
+              Long::class -> json.getDouble(jsonName).toLong()
+              JSONObject::class -> json.getJSONObject(jsonName)
+              JSONArray::class -> json.getJSONArray(jsonName)
+              List::class -> {
+                val jsonArray = json.getJSONArray(jsonName)
+                val list = ArrayList<Any?>()
+                val innerType = cache.innerType!!
+                (0 until jsonArray.length()).forEach {
+                  list.add(
+                    when (innerType) {
+                      Boolean::class -> jsonArray.getBoolean(it)
+                      String::class -> jsonArray.getString(it)
+                      Int::class -> jsonArray.getInt(it)
+                      Float::class -> jsonArray.getDouble(it).toFloat()
+                      Double::class -> jsonArray.getDouble(it)
+                      Long::class -> jsonArray.getDouble(it).toLong()
+                      JSONObject::class -> jsonArray.getJSONObject(it)
+                      JSONArray::class -> jsonArray.getJSONArray(it)
+                      else -> if (
+                        jsonArray.optJSONObject(it) != null) {
+                        deserialize(
+                          innerType,
+                          jsonArray.getJSONObject(it),
+                          serializers
+                        )
+                      } else
+                        null
+                    }
+                  )
+                }
+                list
               }
-              prop.returnType.isMarkedNullable -> null
-              else -> throw Exception("Deserialization key not found : " + type.simpleName + " - " + name)
+              Map::class -> {
+                val jsonObject = json.getJSONObject(jsonName)
+                val map = HashMap<String, Any?>()
+                val valueType = cache.innerType!!
+                jsonObject.keys().forEach {
+                  map[it] = when (valueType) {
+                    Boolean::class -> jsonObject.getBoolean(it)
+                    String::class -> jsonObject.getString(it)
+                    Int::class -> jsonObject.getInt(it)
+                    Float::class -> jsonObject.getDouble(it).toFloat()
+                    Double::class -> jsonObject.getDouble(it)
+                    Long::class -> jsonObject.getDouble(it).toLong()
+                    JSONObject::class -> jsonObject.getJSONObject(it)
+                    JSONArray::class -> jsonObject.getJSONArray(it)
+                    else -> {
+                      if (JsonUtil.isJsonObject(jsonObject, it)) {
+                        val temp = deserialize(
+                          valueType,
+                          jsonObject.getJSONObject(it),
+                          serializers
+                        )
+                        temp
+                      } else
+                        null
+                    }
+                  }
+                }
+
+                map
+              }
+              Set::class -> {
+                val jsonArray = json.getJSONArray(jsonName)
+                val list = HashSet<Any?>()
+                val innerType = cache.innerType!!
+                (0 until jsonArray.length()).forEach {
+                  list.add(
+                    when (innerType) {
+                      Boolean::class -> jsonArray.getBoolean(it)
+                      String::class -> jsonArray.getString(it)
+                      Int::class -> jsonArray.getInt(it)
+                      Float::class -> jsonArray.getDouble(it).toFloat()
+                      Double::class -> jsonArray.getDouble(it)
+                      Long::class -> jsonArray.getDouble(it).toLong()
+                      JSONObject::class -> jsonArray.getJSONObject(it)
+                      JSONArray::class -> jsonArray.getJSONArray(it)
+                      else -> if (
+                        jsonArray.optJSONObject(it) != null) {
+                        deserialize(
+                          innerType,
+                          jsonArray.getJSONObject(it),
+                          serializers
+                        )
+                      } else
+                        null
+                    }
+                  )
+                }
+                list
+              }
+              else -> {
+                deserialize(fieldType, json.getJSONObject(jsonName), serializers)
+              }
+
             }
-          } else {
-
-            if (serializers.containsKey(fieldType)) {
-              Log.e("serilizer", "" + fieldType)
-              serializers[fieldType]!!.deserialize(
-                fieldType,
-                json.getJSONObject(jsonName),
-                serializers
-              )
-            } else
-              when (fieldType) {
-
-                Boolean::class -> json.getBoolean(jsonName)
-                String::class -> json.getString(jsonName)
-                Int::class -> json.getInt(jsonName)
-                Float::class -> json.getDouble(jsonName).toFloat()
-                Double::class -> json.getDouble(jsonName)
-                Long::class -> json.getDouble(jsonName).toLong()
-                JSONObject::class -> json.getJSONObject(jsonName)
-                JSONArray::class -> json.getJSONArray(jsonName)
-                List::class -> {
-                  val jsonArray = json.getJSONArray(jsonName)
-                  val list = ArrayList<Any>()
-                  if (prop.returnType.arguments[0].type != null) {
-                    val innerType = (prop.returnType.arguments[0].type)?.classifier as KClass<*>
-
-                    val add = java.util.ArrayList::class.declaredFunctions.find {
-                      it.name == "add" && it.parameters.size == 2
-                    }
-                    (0 until jsonArray.length()).forEach {
-
-                      add?.call(
-                        list, when (innerType) {
-                          Boolean::class -> jsonArray.getBoolean(it)
-                          String::class -> jsonArray.getString(it)
-                          Int::class -> jsonArray.getInt(it)
-                          Float::class -> jsonArray.getDouble(it).toFloat()
-                          Double::class -> jsonArray.getDouble(it)
-                          Long::class -> jsonArray.getDouble(it).toLong()
-                          JSONObject::class -> jsonArray.getJSONObject(it)
-                          JSONArray::class -> jsonArray.getJSONArray(it)
-                          else -> if (
-                            jsonArray.optJSONObject(it) != null) {
-                            deserialize(
-                              innerType,
-                              jsonArray.getJSONObject(it),
-                              serializers
-                            )
-                          } else
-                            null
-                        }
-                      )
-                    }
-                  }
-                  list
-                }
-                Map::class -> {
-                  val jsonObject = json.getJSONObject(jsonName)
-
-                  val map = HashMap<String, Any>()
-
-                  if (prop.returnType.arguments[0].type != null && prop.returnType.arguments[1].type != null) {
-                    val keyType = (prop.returnType.arguments[0].type)?.classifier as KClass<*>
-                    val valueType = (prop.returnType.arguments[1].type)?.classifier as KClass<*>
-
-                    if (keyType != String::class) {
-                      throw Exception("Map key type must be string ${keyType.simpleName} - $name")
-                    }
-
-                    val put = java.util.HashMap::class.declaredFunctions.find {
-                      it.name == "put"
-                    }
-
-                    jsonObject.keys().forEach {
-                      put?.call(
-                        map, it, when (valueType) {
-                          Boolean::class -> jsonObject.getBoolean(it)
-                          String::class -> jsonObject.getString(it)
-                          Int::class -> jsonObject.getInt(it)
-                          Float::class -> jsonObject.getDouble(it).toFloat()
-                          Double::class -> jsonObject.getDouble(it)
-                          Long::class -> jsonObject.getDouble(it).toLong()
-                          JSONObject::class -> jsonObject.getJSONObject(it)
-                          JSONArray::class -> jsonObject.getJSONArray(it)
-                          else -> {
-                            if (JsonUtil.isJsonObject(jsonObject, it)) {
-                              val temp = deserialize(
-                                valueType,
-                                jsonObject.getJSONObject(it),
-                                serializers
-                              )
-                              temp
-                            } else
-                              null
-                          }
-                        }
-                      )
-                    }
-
-                  }
-                  map
-                }
-                Set::class -> {
-                  val jsonArray = json.getJSONArray(jsonName)
-
-                  val list = HashSet<Any>()
-
-                  if (prop.returnType.arguments[0].type != null) {
-                    val innerType = (prop.returnType.arguments[0].type)?.classifier as KClass<*>
-
-                    val add = java.util.HashSet::class.declaredFunctions.find {
-                      it.name == "add"
-                    }
-                    (0 until jsonArray.length()).forEach {
-
-                      add?.call(
-                        list, when (innerType) {
-                          Boolean::class -> jsonArray.getBoolean(it)
-                          String::class -> jsonArray.getString(it)
-                          Int::class -> jsonArray.getInt(it)
-                          Float::class -> jsonArray.getDouble(it).toFloat()
-                          Double::class -> jsonArray.getDouble(it)
-                          Long::class -> jsonArray.getDouble(it).toLong()
-                          JSONObject::class -> jsonArray.getJSONObject(it)
-                          JSONArray::class -> jsonArray.getJSONArray(it)
-                          else -> if (
-                            jsonArray.optJSONObject(it) != null) {
-                            deserialize(
-                              innerType,
-                              jsonArray.getJSONObject(it),
-                              serializers
-                            )
-                          } else
-                            null
-                        }
-                      )
-                    }
-                  }
-                  list
-                }
-                else -> {
-                  deserialize(fieldType, json.getJSONObject(jsonName), serializers)
-                }
-
-              }
-          }
-      }
-
+        }
     }
+
     return out
   }
 
+  class ValueCache(
+    val constructor: KFunction<Any>,
+    val props: ArrayList<KMutableProperty<*>> = ArrayList(),
+    val values: ArrayList<Value> = ArrayList()
+  ) {
+    class Value(
+      val name: String,
+      val jsonName: String,
+      val jsonPath: String,
+      val type: KClass<*>,
+      val default: Any?,
+      val innerType: KClass<*>?
+    )
+  }
 }
