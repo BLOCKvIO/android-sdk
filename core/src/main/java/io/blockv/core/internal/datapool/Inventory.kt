4368bbae25da9e390d5de15a513a988b697f41dc
LordCheddar <lordcheddar@gmail.com>
2019-04-24 22:44:57 +0200

    add datapool invenotry

---
+++ core/src/main/java/io/blockv/core/internal/datapool/Inventory.kt
@@ -0,0 +1,13 @@
+package io.blockv.core.internal.datapool
+
+import io.blockv.common.model.Message
+import io.blockv.common.model.Vatom
+import io.reactivex.Flowable
+
+interface Inventory {
+
+  fun getRegion(id: String = "."): Flowable<Message<Vatom>>
+
+  fun dispose()
+
+}
