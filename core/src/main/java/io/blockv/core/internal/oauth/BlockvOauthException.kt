3df52f665bb977be0a39e14de608b99339337825
Sean van Duffelen <lordcheddar@gmail.com>
2019-06-06 20:45:10 +0200

    Fix: OAuth login delay (#154)

--- core/src/main/java/io/blockv/core/internal/oauth/BlockvOauthException.kt
+++ core/src/main/java/io/blockv/core/internal/oauth/BlockvOauthException.kt
@@ -6,6 +6,7 @@ class BlockvOauthException(val error: Error) : Exception(error.message) {
     OAUTH_UNAVAILABLE("Error loading Blockv OAuth"),
     STATE_CHANGED("OAuth response has been tampered with"),
     ACCESS_DENIED("The user declined the permission"),
+    USER_CANCEL("The OAuth activity was closed prior to finishing."),
     UNKNOWN("An error has occurred");
 
     fun exception(): BlockvOauthException {
