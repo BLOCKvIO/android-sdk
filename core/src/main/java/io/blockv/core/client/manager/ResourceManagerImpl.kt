47fdc499a9842cc287f71933ab47fc04e8f4fc4d
LordCheddar <lordcheddar@gmail.com>
2019-06-18 13:08:03 +0200

    remove line

--- core/src/main/java/io/blockv/core/client/manager/ResourceManagerImpl.kt
+++ core/src/main/java/io/blockv/core/client/manager/ResourceManagerImpl.kt
@@ -33,7 +33,6 @@ import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.util.*
 
-
 class ResourceManagerImpl(
   cacheDir: File,
   private val encoder: ResourceEncoder,
