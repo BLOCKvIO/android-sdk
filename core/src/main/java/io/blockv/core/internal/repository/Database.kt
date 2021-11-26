244c797a23ca93c680ef7c6b2a89969d35855179
emmanuelconradie <emmanuelconradie@gmail.com>
2021-11-26 23:43:18 +0200

    -change Database.kt version to 6

--- core/src/main/java/io/blockv/core/internal/repository/Database.kt
+++ core/src/main/java/io/blockv/core/internal/repository/Database.kt
@@ -13,7 +13,7 @@ import io.blockv.common.model.Face
 import io.blockv.common.model.Vatom
 import io.blockv.core.internal.repository.dao.VatomDao
 
-@Database(entities = [Vatom::class, Face::class, Action::class], version = 5)
+@Database(entities = [Vatom::class, Face::class, Action::class], version = 6)
 @TypeConverters(
   JsonConverter::class,
   StringListConverter::class,
